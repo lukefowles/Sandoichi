39f567527e09f70ad321d0c900d0f94345f12fd1
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-26 14:57:41 +0000

    functioning react-redux

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Form from './components/Form/Form';
+import Sandwiches from './components/Sandwiches/Sandwiches'
+import { useDispatch } from 'react-redux';
+// will allow us to dispatch an action 
 
 const App = () => {
+
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     dispatch(getOrders());
+    // }, [dispatch]);
+
     return(
         <div>
-            <h1>App</h1>
+            <div>
+                <Sandwiches />
+            </div>
+            <div>
+                <Form />
+            </div>
         </div>
     )
 }
---
+++ client-side/src/components/Sandwiches/Sandwiches.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Sandwiches = () => {
+
+
+    return (
+        <>
+            <h1>Sandwiches</h1>
+        </>
+    );
+}
+
+export default Sandwiches;
