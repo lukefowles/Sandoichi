5dae0441b900e625eea465122cc68f6274f4866f
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-27 18:37:48 +0000

    Sandwiche, sides and drinks added in flexbox

--- client-side/components/Sandwich.jsx
+++ client-side/components/Sandwich.jsx
@@ -6,32 +6,32 @@ const Sandwich = () => {
   return (
     <div id="sandwich" className={styles.container}>
       <div className={styles.row}>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/beef.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Roast Beef & Horse Radish</h1>
           <span className={styles.price}>£4.50</span>
         </div>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/salmon.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Smoked Salmon & Avocado</h1>
           <span className={styles.price}>£5.75</span>
         </div>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/chicken.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Coronation Chicken</h1>
           <span className={styles.price}>£3.50</span>
         </div>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/bacon.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Bacon, Lettuce & Tomato</h1>
           <span className={styles.price}>£4.50</span>
         </div>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/shrimp.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Shrimp & Mayonnaise</h1>
           <span className={styles.price}>£6.25</span>
         </div>
-        <div className={styles.wrapper}>
+        <div className={styles.card}>
           <Image src="/img/custom.png" alt="" width="300px" height="200px" />
           <h1 className={styles.title}>Make Your Own</h1>
           <span className={styles.price}>£5.00</span>
--- client-side/components/Sides.jsx
+++ client-side/components/Sides.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import styles from "../styles/Sides.module.css";
+import Image from "next/image";
+
 
 const Sides = () => {
   return ( 
     <div id="sides" className={styles.container}>
-      Hello
+      <div className={styles.row}>
+        <div className={styles.card}>
+          <Image src="/img/cajun.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Cajun Spiced Fries</h1>
+          <span className={styles.price}>£4.50</span>
+        </div>
+        <div className={styles.card}>
+          <Image src="/img/kale.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Organic Kale Crisps</h1>
+          <span className={styles.price}>£2.25</span>
+        </div>
+        <div className={styles.card}>
+          <Image src="/img/sweet.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Sweet Potato Crisps</h1>
+          <span className={styles.price}>£3.00</span>
+        </div>
+        <div className={styles.card}>
+          <Image src="/img/coleslaw.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Coleslaw & Blue Cheese</h1>
+          <span className={styles.price}>£6.50</span>
+        </div>
+        <div className={styles.card}>
+          <Image src="/img/fruit.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Cherry Pecan Fruit Salad</h1>
+          <span className={styles.price}>£5.50</span>
+        </div>
+        <div className={styles.card}>
+          <Image src="/img/soup.png" alt="" width="300px" height="200px" />
+          <h1 className={styles.title}>Soup of the Day</h1>
+          <span className={styles.price}>£3.00</span>
+        </div>
+      </div>
     </div>
 )    
 };
