ff197ec7473c580a2d85489b9cf94db150e47586
andyjowles <luke.fowles15@gmail.com>
2022-01-31 15:04:22 +0000

    fixed bug, now allowing repeat orders

--- server-side/routes/order.js
+++ server-side/routes/order.js
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import Order from '../models/order-model.js';
 import User from '../models/user-model.js';
 import mongoose from 'mongoose';
+import auth from './tokenRoute.js'
 
 const orderRouter = new Router();
 
