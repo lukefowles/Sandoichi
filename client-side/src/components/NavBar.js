d32818a972906aa2f87a667528aaf8ce3706eac2
helboi4 <h.hopecarter@gmail.com>
2022-02-01 01:18:13 +0000

    added carousel functionality

---
+++ client-side/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+  return <div className='navbar'>
+      <nav>
+        <h1 className=".nav-item">Sandoichi</h1>
+        <div className="nav-buttons">
+        <h2 className=".nav-item">User</h2>
+        <h2 className=".nav-item">Cart</h2>
+        </div>
+      </nav>
+  </div>;
+}
+
+export default NavBar;
--- client-side/src/containers/MainPage.js
+++ client-side/src/containers/MainPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import "../styles/mainpage.css"
+import NavBar from "../components/NavBar"
+import Carousel, {CarouselItem} from '../components/Carousel';
+import "../styles/carousel.css"
 
 function MainPage() {
   return <div>
-      <h1>Sandoichi</h1>
-      <img src=""/>
+      <NavBar/>
+      <Carousel>
+          <CarouselItem>Item 1</CarouselItem>
+          <CarouselItem>Item 2</CarouselItem>
+          <CarouselItem>Item 3</CarouselItem>
+      </Carousel>
   </div>;
 }
 
