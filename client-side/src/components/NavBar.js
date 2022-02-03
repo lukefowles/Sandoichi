dc619a9e5ab5c0f981d47ee23e6f957e510ab994
helboi4 <h.hopecarter@gmail.com>
2022-02-03 20:42:09 +0000

    added icons to login and cart

--- client-side/src/components/NavBar.js
+++ client-side/src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import shoppingCart from "../img/img/shopping-cart.png"
+import userIcon from "../img/img/outline_person_outline_black_48dp.png"
 
 function NavBar() {
   return <div className='navbar'>
       <nav>
         <h1 className=".nav-item">Sandoichi</h1>
         <div className="nav-buttons">
-        <h2 className=".nav-item">User</h2>
-        <h2 className=".nav-item">Cart</h2>
+        <h2 className=".nav-item"><img src={userIcon} alt="login" width="57"/></h2>
+        <h2 className=".nav-item"><img src={shoppingCart} alt="cart" width="50"/></h2>
         </div>
       </nav>
   </div>;
