38adcc1212b0ce33f7ccecf8859c31d2da509fe0
helboi4 <h.hopecarter@gmail.com>
2022-02-14 15:08:10 +0000

    added button on final checkout page

--- client-side/src/components/CompleteDisplay.js
+++ client-side/src/components/CompleteDisplay.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import check from "../img/img/check.png"
+import {Link} from "react-router-dom"
+import "../styles/cartPage.css"
 
 function CompleteDisplay() {
   return (
-    <div>
+    <div className='complete-display'>
       <h1>Thank you for your order!</h1>
       <h3>Your order will be with you in approximately 30 minutes.</h3>
       <img src={check} alt="check" />
-
+      <button><Link to="/" >Return to main page</Link></button>
     </div>
   )
 }
